36d47ada321be6e0f9bc52bb62f4ed7ca4addfee
praveen-gritwizz <praveengritwiz@gmail.com>
2026-04-20 21:04:58 +0530

    changes in the model assigning

--- backend/app/models/expense.py
+++ backend/app/models/expense.py
@@ -1,6 +1,5 @@
 from typing import Optional
-from datetime import datetime, date as dt_date
-from decimal import Decimal
+from datetime import datetime, date as dt_date, timezone
 
 from sqlmodel import SQLModel, Field
 
@@ -8,12 +7,19 @@ from sqlmodel import SQLModel, Field
 class Expense(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    amount: Decimal = Field(nullable=False)
+    amount: float = Field(nullable=False)
+
     category: str = Field(nullable=False, index=True)
     description: Optional[str] = None
 
     date: dt_date = Field(nullable=False, index=True)
 
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
 
-    idempotency_key: Optional[str] = Field(default=None, index=True, unique=True)
+    idempotency_key: str = Field(
+        nullable=False,
+        index=True,
+        unique=True
+    )
